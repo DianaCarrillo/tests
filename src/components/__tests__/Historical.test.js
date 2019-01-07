db3eba8aaf627d3f0056015a8cf5a656b89380d7
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2019-01-07 06:46:20 -0600

    tests

--- src/components/__tests__/Historical.test.js
+++ src/components/__tests__/Historical.test.js
@@ -1,13 +1,27 @@
 /* eslint-disable no-undef*/
 
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Historical from "../Historical/Historical";
 import HistoricalBody from "../Historical/HistoricalBody";
-
-const wrapper = shallowMount(Historical);
+import HistoricalFooter from "../Historical/HistoricalFooter";
 
 describe("Historical", () => {
-  it("Has HistoricalBody as a child", () => {
+ const wrapper = mount(Historical);
+
+  it("Has three children components", () => {
+    const children = wrapper.vm.$children;
+    expect(children.length).toBe(3);
+
+    expect(wrapper.find(HistoricalFooter).exists()).toBe(true);
     expect(wrapper.find(HistoricalBody).exists()).toBe(true);
+  });
+});
+
+describe("HistoricalFooter", () => {
+const wrapper = mount(HistoricalFooter);
+
+  it("has a class", () => {
+    expect(wrapper.contains("a")).toBe(true);
+    expect(wrapper.find("a").classes("historical-footer")).toBe(true);
   });
 });
--- src/components/__tests__/SomeComponent.test.js
+++ src/components/__tests__/SomeComponent.test.js
@@ -1,22 +1,16 @@
 /* eslint-disable no-undef*/
+import { shallowMount } from "@vue/test-utils";
+import SomeComponent from "../SideBar/SomeComponent.vue";
 
-// import { shallowMount } from "@vue/test-utils";
-// import someComponent from "../SideBar/SomeComponent.vue";
+describe("someComponent.vue", () => {
 
-describe("SomeComponent.vue", () => {
-  it("works", () => {
-    expect(true).toBe(true);
+  it("displays a welcome message", () => {
+    const msg = "Welcome to Your Vue.js App";
+    const wrapper = shallowMount(SomeComponent, {
+      propsData: {
+        msg: msg
+      }
+    });
+  expect(wrapper.find("h1").text()).toBe("Welcome to Your Vue.js App");
   });
-
-  // it("displays a welcome message", () => {
-  //   const msg = "Welcome to Your Vue.js App";
-  //   const wrapper = shallowMount(someComponent, {
-  //     props: {
-  //       msg: msg
-  //     }
-  //   });
-
-  // console.log(wrapper.html());
-  // expect(wrapper.find("h1").text()).toBe("Welcome to Your Vue.js App");
-  // });
 });

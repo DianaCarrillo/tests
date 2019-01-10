83cd316422eab658e227033ea6cb4e3206b2c702
Diana Carrillo <dianacarrillo.psic@outlook.com>
2019-01-10 09:18:11 -0600

    updating

--- src/components/__tests__/SomeComponent.test.js
+++ src/components/__tests__/SomeComponent.test.js
@@ -3,7 +3,6 @@ import { shallowMount } from "@vue/test-utils";
 import SomeComponent from "../SideBar/SomeComponent.vue";
 
 describe("someComponent.vue", () => {
-
   it("displays a welcome message", () => {
     const msg = "Welcome to Your Vue.js App";
     const wrapper = shallowMount(SomeComponent, {
@@ -11,6 +10,6 @@ describe("someComponent.vue", () => {
         msg: msg
       }
     });
-  expect(wrapper.find("h1").text()).toBe("Welcome to Your Vue.js App");
+    expect(wrapper.find("h1").text()).toBe("Welcome to Your Vue.js App");
   });
 });

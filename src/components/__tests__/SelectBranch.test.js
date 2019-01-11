3c146ee6359dfea57e1a00ec26e079469edffe10
Diana Carrillo <dianacarrillo.psic@outlook.com>
2019-01-11 17:40:17 -0600

    updating tests

--- src/components/__tests__/SelectBranch.test.js
+++ src/components/__tests__/SelectBranch.test.js
@@ -41,6 +41,22 @@ describe("SelectBranch", () => {
     expect(trueCount).toEqual(2);
   });
 
+  it("has one non-active company", () => {
+    const { wrapper } = render(SelectBranch);
+    let nonActive;
+    if (wrapper.vm.companiesFromServer.active === false) {
+      nonActive++;
+    }
+    // for (let j = 0; j < wrapper.vm.companiesFromServer.length; j++) {
+    //   if (!wrapper.vm.companiesFromServer[j].active) {
+    //     nonActive++;
+    //   }
+    // }
+    expect(nonActive).toEqual(1);
+    const disabled = wrapper.findAll("button:disabled");
+    expect(disabled.length).toEqual(nonActive);
+  });
+
   it("It has two component children", () => {
     const { wrapper } = render(SelectBranch);
     const children = wrapper.vm.$children;
@@ -49,12 +65,11 @@ describe("SelectBranch", () => {
     expect(wrapper.find(CompanyList).exists()).toBe(true);
     expect(wrapper.find(BranchesList).exists()).toBe(true);
   });
-
   // it("Highlights the currently selected item");
 });
 
 describe("BranchesList", () => {
-  it("Contains hides the BranchList component", async () => {
+  it("Hides the BranchList component", async () => {
     const { wrapper } = render(BranchesList, {
       propsData: {
         isHidden: true,
@@ -80,6 +95,7 @@ describe("BranchesList", () => {
     const div = wrapper.findAll("div").at(0);
     expect(div.classes()).toContain("is-hidden");
   });
+
   it("It renders braches column correctly", async () => {
     const { wrapper, getByText } = render(SelectBranch);
     const company = wrapper.vm.companiesFromServer[0].name;
@@ -90,32 +106,3 @@ describe("BranchesList", () => {
     expect(div.classes("is-hidden")).toBe(false);
   });
 });
-
-// it("it highlights the currently selected item", async () => {
-//   const { wrapper, getByText } = render(SelectBranch);
-
-//   const item1Name = wrapper.vm.companiesFromServer[0].name;
-//   const item1 = getByText(item1Name);
-
-//   await fireEvent.click(item1);
-
-//   const activeItem = wrapper.find(".active");
-//   expect(activeItem.text()).toContain("Empresa Alpha");
-
-// const activeItem3 = wrapper.find(".active");
-// expect(activeItem3.text()).toContain("Almacén Sonora Grill");
-// await fireEvent.click(item2);
-
-// const activeItem2 = wrapper.find(".active");
-// expect(activeItem2.text()).toContain("Beta");
-// });
-
-// describe("BranchesListItem", () => {
-//   it("Renders Branches column correcly", () => {
-//     const wrapper = mount(BranchesListItem);
-//     const buttonsArray = wrapper.findAll("button");
-//     expect(buttonsArray.contains("p")).toBe(true);
-// wrapper.vm.handleCompanySelect(wrapper.vm.companiesFromServer[0]);
-// expect(wrapper.find("button").classes("select-item")).toBe(true);
-// });
-// });
